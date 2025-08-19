aea0313c49992cb0da1fc3750761e82a0890cdc1
joe95-ux <ogorktabi@gmail.com>
2025-08-19 23:16:36 +0200

    add common modal

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,13 +1,12 @@
-import Logo from "@/components/Logo";
-import React, { ReactNode } from "react";
+import React from "react";
 
-function layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center">
-      <Logo />
-      <div className="mt-12">{children}</div>
-    </div>
-  );
+const layout = ({children}: {children: React.ReactNode})=>{
+    return(
+        <main className="h-full flex justify-center items-center">
+            {children}
+        </main>
+    )
+
 }
 
 export default layout;
